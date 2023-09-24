e374125e4a7a38d127d901ae12368d01e788c89a
negar mokhtari <negar.mmkhtri@gmail.com>
2023-09-24 10:47:37 +0330

    add a label to test one

--- src/components/Test1/Test1.tsx
+++ src/components/Test1/Test1.tsx
@@ -1,32 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { z } from 'zod'
-import {zodResolver} from '@hookform/resolvers/zod'
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const schma=z.object({
-    name:z.string().min(3,{message:'hrof bishtar'}).max(8,{message:'hrof kamtar'}),
-    pass:z.string().min(8,{message:'ramz sade'})
-})
+const schma = z.object({
+  name: z
+    .string()
+    .min(3, { message: "hrof bishtar" })
+    .max(8, { message: "hrof kamtar" }),
+  pass: z.string().min(8, { message: "ramz sade" }),
+});
 
-type Checking=z.infer<typeof schma>;
+type Checking = z.infer<typeof schma>;
 
-const Test1=()=>{
-
-
-    const[register,setRegister]=useState<Checking>()
+const Test1 = () => {
+  const [register, setRegister] = useState<Checking>();
   return (
     <form>
-        <div>
-            <label htmlFor="name">Name</label>
-            <input id='name' type="text" />
-        </div>
-        <div>
-             <label htmlFor="psss">Password</label>
-             <input id='pass' type="password" />
-        </div>
-        <button> SABT</button>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" />
+      </div>
+      <div>
+        <label htmlFor="psss">Password</label>
+        <input id="pass" type="password" />
+      </div>
+      <div>
+        <label htmlFor=""></label>
+      </div>
+      <button> SABT</button>
     </form>
   );
-}
+};
 
-export default Test1
+export default Test1;
